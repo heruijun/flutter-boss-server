43d96e17e44b78c531525130374aab7f97b69f86
何瑞君 <heruijun@heruijundeMacBook-Pro.local>
2018-12-30 22:42:37 +0800

    实现分页查询

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,27 @@ const Router = require('koa-router')
 const mongoose = require('mongoose')
 const router = new Router()
 
-router.get('/jobs/all', async (ctx, next) => {
+router.get('/jobs/list/:page', async (ctx, next) => {
+  const curPage = Number(ctx.params.page)
+  const pageSize = 10
   const Job = mongoose.model('Job')
+  const total = await Job.find({}).count()
+  const totalPage = Math.floor((total + pageSize - 1) / pageSize)
+  const hasNextPage = curPage < totalPage ? true : false
   const jobs = await Job.find({}).sort({
     'meta.createdAt': -1
-  })
+  }).skip((curPage - 1) * pageSize).limit(pageSize)
 
   ctx.body = {
-    jobs
+    'data': {
+      jobs,
+      'pages': {
+        curPage,
+        totalPage,
+        hasNextPage,
+        total
+      }
+    }
   }
 })
 
